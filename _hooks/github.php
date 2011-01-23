cdd81fedba9c9ab40f011ceb1defd35ccd9ad7ed
Sean Coates <sean@seancoates.com>
2011-01-22 21:36:51 -0500

    git hook with proper env

--- _hooks/github.php
+++ _hooks/github.php
@@ -1,2 +1,6 @@
 <?php
-echo `cd /src/phpcon-web && env git pull`;
+$gitpath = '/usr/local/bin/git';
+header("Content-type: text/plain");
+system("/usr/bin/env -i HOME=/var/www/vhosts/phpcon.org {$gitpath} pull 2>&1");
+echo "\nDone.\n";
+
