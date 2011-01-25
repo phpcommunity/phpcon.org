bc9013ecd21d4af2711457ea40f25be0de4b3e59
Ben Ramsey <ramsey@php.net>
2011-01-25 04:20:43 +0000

    Simplified front controller code and added a 404 page.

--- index.php
+++ index.php
@@ -1,41 +1,32 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+
 $uri = $_SERVER['REQUEST_URI'];
-
-function perm_redirect($url) {
-    header("HTTP/1.1 301 Moved Permanently");
-    header("Location: $url");
-    exit();
+if (substr($uri, -1) == '/') {
+    $uri = substr($uri, 0, -1);
 }
 
-$resources = array(
-    array('^/$', false, 'home.html'),
-    array('^/sponsorships$', false, 'sponsorships.html'),
-
-    //redirects
-    array('^/index\.html$', true, '/'),
-    array('^/sponsorships\.html$', true, '/sponsorships'),
-);
-
-$page = '';
-
-foreach ($resources as $rsrc) {
-    if (preg_match( '|' . $rsrc[0] . '|', $uri)) {
-        if ($rsrc[1] == false) {
-            $page = $rsrc[2];
-            break;
-        } else {
-            perm_redirect($rsrc[2]);
-        }
-    }
+$requestedPage = substr($uri, strrpos($uri, '/') + 1);
+if (strtolower(substr($requestedPage, -5)) == '.html') {
+    header('Location: ' . substr($uri, 0, strrpos($uri, '/')) . '/' . substr($requestedPage, 0, -5), true, 301);
+    exit;
+}
+if (strtolower($requestedPage) == 'index' || strtolower($requestedPage) == 'index.php') {
+    header('Location: /', true, 301);
+    exit;
+}
+if (!ctype_alnum($requestedPage)) {
+    $requestedPage = '404';
+}
+if (strlen($requestedPage) == 0) {
+    $requestedPage = 'home';
 }
 
-if ($page == '') {
-    header("HTTP/1.1 404 Not Found");
-    die('Page not found.');
-} else {
-    $page = 'resources/' . $page;
+if (!(include 'resources/' . $requestedPage . '.html')) {
+    header('HTTP/1.0 404 Not Found');
+    include 'resources/404.html';
 }
 
-include('templates/page.php');
-
+include 'templates/page.php';
